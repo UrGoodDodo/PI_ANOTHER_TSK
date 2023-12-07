dd21cb4122f62b0e71e24ac24e6cc08deff8e01b
PrAl4 <igorsobaka4@gmail.com>
2023-12-07 19:30:53 +0300

    com 4

--- PI_ANOTHER_TSK/Program.cs
+++ PI_ANOTHER_TSK/Program.cs
@@ -5,7 +5,7 @@ class MatrixOp
 {
     double[,] first;
     double[,] second;
-    public MatrixOp(double[,] first, double[,] second) 
+    public MatrixOp(double[,] first, double[,] second)
     {
         this.first = first;
         this.second = second;
@@ -28,16 +28,17 @@ class MatrixOp
         return r;
     }
 
-    public double[,] Sum() 
+    public double[,] Sum()
     {
         var t = first;
         if (first.GetLength(1) != second.GetLength(1) && first.GetLength(0) != second.GetLength(0)) throw new Exception("Матрицы нельзя сложить");
-        for (int i = 0; i < first.GetLength(0); i++) 
+        for (int i = 0; i < first.GetLength(0); i++)
         {
-            for (int j = 0; j < first.GetLength(1); j++) 
+            for (int j = 0; j < first.GetLength(1); j++)
             {
-                t[i, j] += second[i, j]; 
+                t[i, j] += second[i, j];
             }
+        }
         }
         return t;
     }
